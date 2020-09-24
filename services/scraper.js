4e9725b9b864eb59a82605f424d72f1e7c85170a
mccanndomi <domnic.mccann@outlook.com>
2020-09-24 15:58:09 +1200

    fetching all main post links works

--- services/scraper.js
+++ services/scraper.js
@@ -11,11 +11,30 @@ exports.posts = async () => {
   await page.goto(HOME_URL);
 
   //fetch all links for messages
-  const pages = await page.evaluate(() =>
+  const postLinks = await page.evaluate(() =>
     Array.from(document.querySelectorAll("li a")).map((entry) => entry.href)
   );
 
+  var mainPosts = [];
+  var mainPostsIndex = 0;
+  //sort through all links and remove the first of each section
+  //this is to remove the comment links that arn't needed
+  //we just need the main post link.
+  var i;
+  for (i = 0; i < postLinks.length; i++) {
+    //if the first link add to posts
+    if (i == 0) {
+      mainPosts[mainPostsIndex++] = postLinks[i];
+    }
+    //if previous link is blank then add this link as
+    //it is start of new thread
+    if (postLinks[i - 1] == "") {
+      mainPosts[mainPostsIndex++] = postLinks[i];
+    }
+  }
+
   await browser.close();
 
-  return pages;
+  //return all main post links
+  return mainPosts;
 };
